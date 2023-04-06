1786154f8ec2682bdfcae224a9ede3c9d67c11ec
reestify <reestify@gmail.com>
2023-04-07 01:42:38 +0600

    User registration & pass hashing

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class AuthController
+{
+    public function __construct(private readonly Twig $twig,private readonly EntityManager $entityManager)
+    {
+
+    }
+
+    public function loginView(Request $request, Response $response): Response
+    {
+        return $this->twig->render($response, 'auth/login.twig');
+    }
+
+    public function registerView(Request $request, Response $response): Response
+    {
+        return $this->twig->render($response, 'auth/register.twig');
+    }
+
+    public function register(Request $request, Response $response)
+    {
+        $data = $request->getParsedBody();
+
+        $user = new User();
+
+        $user->setName($data['name']);
+        $user->setEmail($data['email']);
+        $user->setPassword(password_hash($data['password'],PASSWORD_BCRYPT,['cost'=>13]));
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        
+        return $response;
+    }
+
+    public function loginVie(Request $request, Response $response)
+    {
+        return $this->twig->render($response, 'auth/login.twig');
+    }
+}
